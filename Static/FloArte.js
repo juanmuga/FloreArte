f759e7d00de00767958c77a11175480ba4b5f952
Juan Muga <juanmuga@Juan-Mugas-MBP.fibertel.com.ar>
2016-03-04 16:05:30 -0300

    contacto

--- Static/FloArte.js
+++ Static/FloArte.js
@@ -7,7 +7,9 @@ $(document).ready(function(){
     $('.collapsible').collapsible({
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
- 
+
+
+    
 
     
 
